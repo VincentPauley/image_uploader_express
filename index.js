6a20d629e18c210c5356b733e5fa21c58acff788
Vincent Pauley <vinniepauley@gmail.com>
2017-04-24 20:50:48 -0500

    upload working, no error handling or securtiy yet

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 var path = require( 'path' ),
+    multer = require( 'multer' ), // allows parsing of multipart requests
     express = require( 'express' ),
     app = express();
 
+
+// serve public directory
 app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+var storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    callback( null, 'public/uploads' );
+  },
+  filename: function (request, file, callback) {
+    console.log(file);
+    callback(null, file.originalname)
+  }
+});
+
+// set file destination for uploads
+var upload = multer({storage : storage}).single( 'photo' );
+
+app.post('/upload', function(req, res ) {
+    upload(req, res, function( err ) {
+        if( err ) {
+            console.error( 'ERROR OCCURED' );
+            console.error( err );
+            return;
+        }
+        console.log( req.file );
+        res.end( 'File has been uploaded!' );
+        console.log( 'Photo Uploaded' );
+    });
+});
 
 app.listen( 3000 );
 
